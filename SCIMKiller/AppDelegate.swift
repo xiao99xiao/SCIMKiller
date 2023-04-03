0a0807a8ccccce5b37178cc37d675bbf2d259008
Xiao Xiao <xx2004xiamen@gmail.com>
2023-04-03 12:22:45 +0900

    Initial Commit

---
+++ SCIMKiller/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  SCIMKiller
+//
+//  Created by Xiao Xiao on 2023/04/03.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ SCIMKiller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SCIMKiller
+//
+//  Created by Xiao Xiao on 2023/04/03.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
